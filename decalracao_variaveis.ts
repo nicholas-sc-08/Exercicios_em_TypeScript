cb74dc831a816f7f31fb3bcdb68fc9f5f70c42dc
NICHOLAS SERENCOVICH CARVALHO <nicholas_carvalho@estudante.sesisenai.org.br>
2025-07-29 16:39:15 -0300

    declarações de variaveis

---
+++ decalracao_variaveis.ts
@@ -0,0 +1,5 @@
+const text : string = `Declaração de Variaveis`;
+const numero : number = 123;
+const array_de_texto : string[] = ["user 1", "user 2", "user 3"];
+let array_de_numeros : number[] = [];
+let array_misturado : Array<string | number | object> = [];
